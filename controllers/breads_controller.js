75fe2f1200ee5f640b7362137fea05e5b116ad08
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-05-31 19:35:59 -0700

    Added new comments

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,11 +1,14 @@
+// DEPENDENCIES
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread')
 
+//will create new page when entering http://localhost:3003/breads
 breads.get('/', (req, res)=>{
     res.send('This is the index at /breads')
 })
 
+//Will show the bread based on the array in ./models/bread.js
 breads.get('/:arrayIndex', (req, res)=>{
     res.send(Bread[req.params.arrayIndex])
 })
--- server.js
+++ server.js
@@ -6,15 +6,16 @@ require('dotenv').config()
 const port = process.env.port
 const app = express()
 
-// ROUTES
+// ROUTES configration to port 3003
 app.get('/', (req, res)=>{
     res.send('Welcome to an Awesome App about Breads!')
 })
 
-const breadsController = require('./controllers/breads_controller')
+//connection between server.js to /controllers/breads_controller
+const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads',breadsController)
 
-// LISTEN
+// Listen to the port 3003 under .env
 app.listen(port,()=>{
     console.log('listening on port', port)
 })
